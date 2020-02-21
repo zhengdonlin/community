6fe73903e594d5bee5b96a29c0110705331a5b08
zhengdonglin <214879804@qq.com>
2020-02-21 19:05:03 +0800

    add

--- src/main/java/life/majiang/community/community/Controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/community/Controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
 
@@ -27,7 +29,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRedirect_uri(redirectUri);
@@ -36,9 +39,13 @@ public class AuthorizeController {
         accessTokenDTO.setClient_secret(clientSecret);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        System.out.println(user.getBio());
-        return "index";
-
+        //System.out.println(user.getName());
+        //System.out.println(user.getBio());
+        if (user != null) {
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        } else {
+            return "redirect:/";
+        }
     }
 }
